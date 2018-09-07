e3a0c4507a4530fa717aa754900e251baadb75c0
Toddolish <42237447+Toddolish@users.noreply.github.com>
2018-09-07 15:56:04 +1000

    Initial commit

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems; //control the event (button shiz
+using UnityEngine.Audio;
+
+public class MenuHandler : MonoBehaviour {
+
+    #region Variables
+    [Header("OPTIONS")]
+    public Vector2[] res = new Vector2[7];
+    public int resIndex;
+    public bool isFullScreen;
+    public Dropdown resDropDown;
+
+    [Header("Keys")]
+    public KeyCode holdingKey;
+    public KeyCode forward, backward, left, right, jump, crouch, sprint, interact;
+
+    [Header("References")]
+    public bool showOptions;
+    public GameObject mainMenu, optionsMenu;
+    public Slider volumeSlider;
+    public Slider brightnessSlider;
+    public Slider ambientSlider;
+    public AudioSource mainAudio;
+    public Light dirLight;
+
+    [Header("KeyBing Reference")]
+    public Text forwardText;
+    public Text backwardText, leftText, rightText, jumpText, crouchText, sprintText, interactText;
+
+    #endregion
+
+    private void Start()
+    {
+        mainAudio = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>();
+        dirLight = GameObject.Find("Directional Light").GetComponent<Light>();
+        optionsMenu.SetActive(false);
+        forwardText.text = forwardText.ToString();
+
+        #region Set Up Keys
+        //set out the keys to the preset keys we may have saved, else we are going to set the keys to default
+        forward = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Forward", "W"));
+        backward = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Backward", "S"));
+        left = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left", "A"));
+        right = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Right", "D"));
+        jump = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Jump", "Space"));
+        crouch = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Crouch", "C"));
+        sprint = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Sprint", "LeftShift"));
+        interact = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Interact", "E"));
+        #endregion
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+    public void ExitGame()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+        Application.Quit();
+    }
+    public void ToggleOptions()
+    {
+        OptionsToggle();
+    }
+    bool OptionsToggle() //when click on toggle button "options" then "back"
+    {
+        if(showOptions)//if i clicked "Options" button
+        {
+            showOptions = false;
+            mainMenu.SetActive(true);
+            optionsMenu.SetActive(false);
+            return false;
+        }
+        else //if i clicked "Back" button
+        {
+            showOptions = true;
+            mainMenu.SetActive(false);
+            optionsMenu.SetActive(true);
+            brightnessSlider = GameObject.FindGameObjectWithTag("Bslide").GetComponent<Slider>();
+            volumeSlider = GameObject.FindGameObjectWithTag("AudioSlider").GetComponent<Slider>();
+            ambientSlider = GameObject.Find("Ambient").GetComponent<Slider>();
+            resDropDown = GameObject.Find("Dropdown").GetComponent<Dropdown>();
+            ambientSlider.value = RenderSettings.ambientIntensity;
+            brightnessSlider.value = dirLight.intensity;
+            volumeSlider.value = mainAudio.volume;
+            return true;
+        }
+    }
+    public void Volume()
+    {
+        mainAudio.volume = volumeSlider.value;
+    }
+    public void Brightness()
+    {
+        dirLight.intensity = brightnessSlider.value;
+    }
+    public void Ambient()
+    {
+        // RenderSettings.ambientIntensity = ambientSlider.value;
+        RenderSettings.ambientIntensity = ambientSlider.value;
+    }
+    public void Resolution()
+    {
+        resDropDown.value = resIndex;
+        Screen.SetResolution((int)res[resIndex].x,(int)res[resIndex].y,isFullScreen);
+    }
+    public void Save()
+    {
+        PlayerPrefs.SetString("Forward", forward.ToString());
+        PlayerPrefs.SetString("Backward", backward.ToString());
+        PlayerPrefs.SetString("Left", left.ToString());
+        PlayerPrefs.SetString("Right", right.ToString());
+        PlayerPrefs.SetString("Jump", jump.ToString());
+        PlayerPrefs.SetString("Crouch", crouch.ToString());
+        PlayerPrefs.SetString("Sprint", sprint.ToString());
+        PlayerPrefs.SetString("Interact", interact.ToString());
+    }
+    private void OnGUI()
+    {
+        Event e = Event.current;
+        if(forward == KeyCode.None)
+        {
+            Debug.Log("KeyCode " + e.keyCode);
+            if (!(e.keyCode == backward || e.keyCode == left || e.keyCode == right || e.keyCode == jump || e.keyCode == sprint
+                || e.keyCode == crouch || e.keyCode == interact)) 
+            {
+                forward = e.keyCode;
+                holdingKey = KeyCode.None;
+                forwardText.text = forward.ToString();
+            }
+        }
+    }
+    public void Forward()
+    {
+        if (!(backward == KeyCode.None || left == KeyCode.None || right == KeyCode.None || jump == KeyCode.None || sprint == KeyCode.None
+                || crouch == KeyCode.None || interact == KeyCode.None))
+        {
+            holdingKey = forward;
+            forward = KeyCode.None;
+            forwardText.text = forward.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+//this script can be found in the Component section under the option Character Set Up 
+//Mouse Look
+[AddComponentMenu("FirstPerson/Camera Mouse Look")]
+public class MouseLook : MonoBehaviour 
+{
+    #region Variables
+    [Header("Rotational Axis")]
+    public RotationalAxis axis = RotationalAxis.MouseX;
+    //create a public link to the rotational axis called axis and set a defualt axis
+    [Header("sensitivity")]
+    public float sensitivityX = 10f;
+    public float sensitivityY = 10f;
+    //public floats for our x and y sensitivity
+    [Header("Y Rotation Clamp")]
+    public float minimumY = -60f;
+    public float maximumY = 60f;
+    //max and min Y rotation
+    //we will have to invert our mouse position later to calculate our mouse look correctly
+    //float for rotation Y
+    public Transform Player;
+    float rotationY = 0f;
+    #endregion
+    #region Start
+    void Start()
+    {
+        //if our game object has a rigidbody attached to it
+        if (this.GetComponent<Rigidbody>())
+        {
+            //set the rigidbodys freezRotaion to true
+            this.GetComponent<Rigidbody>().freezeRotation = true;
+
+        }
+        Player = GameObject.Find("Player").GetComponent<Transform>();
+    }
+    #endregion
+    #region Update
+    void Update()
+    {
+        switch (axis)
+        {
+            #region MouseXandY
+            //if our axis is set to Mouse X and Y
+            case RotationalAxis.MouseXandY:
+                //float rotation x is equal to our y axis plus the mouse input on the Mouse X times our x sensitivity
+                float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+                //our rotation Y is plus equals our mouse input for Mouse Y times Y sensitivity
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+                //the rotation Y is clamped using Mathf and we are clamping the y rotation to the Y min and Y max
+                rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+                //transform our local position to the nex vector3 rotaion - y rotaion on the x axis and x rotation on the y axis
+                transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+                break;
+            #endregion
+            #region MouseX
+            //we are rotating on the X
+            case RotationalAxis.MouseX:
+                //transform the rotation on our game objects Y by our Mouse input Mouse X times X sensitivity
+                ////////////////(X)/////////////////(Y)/////////////////////(Z)
+                transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+                break;
+            #endregion
+            #region MouseY
+            //we are only rotation on the Y
+            case RotationalAxis.MouseY:
+                //our rotation Y is pulse equals  our mouse input for Mouse Y times Y sensitivity
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+                //the rotation Y is clamped using Mathf and we are clamping the y rotation to the Y min and Y max
+                rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+                //transform our local position to the nex vector3 rotaion - y rotaion on the x axis and local euler angle Y on the y axis
+                transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+                break;
+                #endregion
+        }
+        //Player.transform.rotation = new Vector3(transform.rotation, RotationalAxis.MouseX, 0);
+        //Player.transform.position = RotationalAxis.MouseX;
+    }
+    #endregion
+}
+#region RotationalAxis
+/*
+enums are what we call state value variables 
+they are comma separated lists of identifiers
+we can use them to create Type or Category variables
+meaning each heading of the list is a type or category element that this can be
+eg weapons in an inventory are a type of inventory item
+if the item is a weapon we can equipt it
+if it is a consumable we can drink it
+it runs different code depending on what that objects enum is set to
+you can also have subtypes within those types
+eg weapons are an inventory category or type
+we can then have ranged, melee weapons
+or daggers, short swords, long swords, mace, axe, great axe, war axe and so on
+each Type or Category holds different infomation the game needs like 
+what animation plays, where the hands sit on the weapon, how many hands sit on the weapon and so on
+*/
+//Create a public enum called RotationalAxis
+public enum RotationalAxis
+{
+    MouseXandY, MouseX,
+    MouseY
+}
+
+//Creating an enum out side the script and making it public means it can be asessed anywhere in any script with out reference
+#endregion
+
+
+
+
+
+
+
+
+
+
+
